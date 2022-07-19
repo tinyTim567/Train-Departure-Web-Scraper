c5a6b15bf35ce71896e9e9a0ecc7c190efbc9b60
tinyTim567 <68090142+tinyTim567@users.noreply.github.com>
2022-07-19 13:31:34 +0100

    Add function to get webpages

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -1,2 +1,24 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+
+
+def get_web_page():
+    url = "https://realtime.nationalrail.co.uk/ldbcis/departures.aspx?u=039B1CD1-14D4-4CB9-83B1-A84CC3AEDF83&crs=HUL&H=1080"
+
+    result = requests.get(url)
+    return BeautifulSoup(result.text, "html.parser")
+
+
 def main():
-    print("Hello there!")
+    doc = get_web_page()
+
+    # print(doc.prettify())
+
+    departures = doc.find_all(["tr"], id=re.compile("trainStation.*"))
+    for departure in departures:
+        for item in departure.contents:
+            print(item.text)
+
+
+
